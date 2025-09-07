b19f517ec42907d48f6ffba9d21cc01121d256e8
Junaid <sattijunaid64@gmail.com>
2025-09-07 03:15:42 -0700

    Fix Nweproduct.css import case issue

--- src/Components/Newproduct.jsx
+++ src/Components/Newproduct.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { getProductsByCategory } from "../services/ProductService"
-import "./NewProducts.css"
+import "./Newproduct.css"
 
 const NewProducts = ({ addToCart, addToHeart }) => {
   const [startIndex, setStartIndex] = useState(0)
